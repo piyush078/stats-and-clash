cce5d369355f3112609fde7c6cda85891ca44174
Piyush Madhusudan <piyush078@users.noreply.github.com>
2017-03-17 03:40:39 +0530

    Update player.php

--- player.php
+++ player.php
@@ -8,7 +8,7 @@
         $ch = curl_init();
         $headerArray = array (
             'Accept: application/json',
-            'authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFmNjRiMmRmLWMyMjctNDlhNy1iNDExLTZkOWM1MGFiMThmNyIsImlhdCI6MTQ4OTUyMDUwNSwic3ViIjoiZGV2ZWxvcGVyLzU1NDA0MzYyLTEyZWYtNmRkZi02ZTkxLWZmMWY5YzcwY2U3YyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjkzLjE4OC4xNjAuMTc1IiwiOTMuMTg4LjE2MC4xOTEiXSwidHlwZSI6ImNsaWVudCJ9XX0.h3axkPX5zWyf2hbzww8DIRN-1vhN6kQ9mYZcsgPbsiD1V07oao3alUBjctBa-3GooMgLWXbG3_ZPXnL-MADGaw'
+            'authorization: Bearer <account_key>'
         );
         curl_setopt($ch, CURLOPT_URL, "https://api.clashofclans.com/v1/players/".$tag);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
